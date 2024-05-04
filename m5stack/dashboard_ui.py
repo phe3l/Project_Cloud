9346e2d16d42f0526470a18ff500f81f713adeef
phe3l <146002891+phe3l@users.noreply.github.com>
2024-05-04 19:34:14 +0200

    Last Update

--- m5stack/dashboard_ui.py
+++ m5stack/dashboard_ui.py
@@ -8,7 +8,7 @@ import urequests
 import ntptime
 from machine import RTC
 from libs.image_plus import *
-
+import utime
 
 
 screen = M5Screen()
@@ -28,46 +28,46 @@ env3_temp_label = M5Label('ENV3 Temp: ', x=20, y=30, color=0x000)
 env3_hum_label = M5Label('ENV3 Humidity: ', x=20, y=50, color=0x000)
 gas_label = M5Label('Gas TVOC: ', x=20, y=70, color=0x000)
 motion_label = M5Label('Motion Detected: No', x=20, y=90, color=0x000)
+date_time_label = M5Label('Date & Time: Loading...', x=20, y=110, color=0x000)
 outdoor_temp_label = M5Label('Outdoor Temp:', x=20, y=130, color=0x000)
 outdoor_hum_label = M5Label('Outdoor Humidity.', x=20, y=150, color=0x000)
 
 # WiFi credentials for connecting to the network
 wifi_credentials = [('TP-Link_76C4', '49032826'),('iot-unil', '4u6uch4hpY9pJ2f9')]
-flask_url = "http://130.223.239.227:8080"
+flask_url = "http://192.168.0.103:8080"
+
+public_ip = None
+
 
 def connect_wifi(wifi_credentials):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    
     if wlan.isconnected():
-        get_public_ip()
         return wlan
-
     for ssid, password in wifi_credentials:
-        try:
-            wlan.connect(ssid, password)
-            start_time = time.time()
-            while time.time() - start_time < 10:  # Attendre 10 secondes pour la connexion
-                if wlan.isconnected():
-                    get_public_ip()
-                    return wlan
-                time.sleep(1)
-            wlan.disconnect()  # Déconnecte proprement si la connexion échoue
-        except OSError as e:
-            IPLabel.set_text('Connection error: ' + str(e))
-            continue
+        wlan.connect(ssid, password)
+        start_time = time.time()
+        while not wlan.isconnected() and time.time() - start_time < 10:
+            time.sleep(1)
+        if wlan.isconnected():
+            return wlan
+        wlan.disconnect()
     return None
+
+
+
 
 # Function to get the public IP address of the device
 def get_public_ip():
+    global public_ip
     try:
         response = urequests.get('http://api.ipify.org/?format=text')
         public_ip = response.text
         response.close()
         IPLabel.set_text('Public IP: ' + public_ip)
-        return public_ip
     except Exception as e:
-        return None
+        IPLabel.set_text('Connection error: ' + str(e))
+
 
 def get_current_weather(ip):
     response = None  # Initialiser response ici pour garantir qu'elle est définie avant le bloc try
@@ -90,9 +90,70 @@ def get_current_weather(ip):
         if response:
             response.close()
 
+import socket
+import struct
+
+def get_ntp_time(host):
+    NTP_DELTA = 3155673600  # 1900-01-01 00:00:00 to 2000-01-01 00:00:00
+    NTP_QUERY = b'\x1b' + 47 * b'\0'
+    addr = socket.getaddrinfo(host, 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.sendto(NTP_QUERY, addr)
+        msg, _ = s.recvfrom(48)
+    finally:
+        s.close()
+    val = struct.unpack('!I', msg[40:44])[0]
+    return val - NTP_DELTA
+
+def get_current_time():
+    # Obtenez l'heure NTP
+    ntp_time = get_ntp_time('ch.pool.ntp.org')  # Serveur NTP pour la Suisse
+
+    # Ajoutez le décalage horaire (2 heures en standard, 3 heures en heure d'été)
+    ntp_time += 2 * 3600  # Changez cela en 3 * 3600 si vous êtes en heure d'été
+
+    # Convertissez l'heure NTP en heure locale
+    local_time = utime.localtime(ntp_time)
+
+    # Définissez l'heure du système
+    rtc = RTC()
+    rtc.datetime((local_time[0], local_time[1], local_time[2], 0, local_time[3], local_time[4], local_time[5], 0))
+
+    # Mettez à jour le texte du label avec l'heure et la date
+    date_time_label.set_text('Date & Time: {}/{}/{} {}:{}:{}'.format(local_time[0], local_time[1], local_time[2], local_time[3], local_time[4], local_time[5]))
+
+
+
+def send_data(ip, outdoor_temp, outdoor_humidity):
+    # Obtenez l'heure et la date actuelles
+    local_time = utime.localtime()
+    date = '{:04d}-{:02d}-{:02d}'.format(local_time[0], local_time[1], local_time[2])
+    time = '{:02d}:{:02d}:{:02d}'.format(local_time[3], local_time[4], local_time[5])
+
+    data = {
+        "values": {
+            "indoor_temp": env3_0.temperature,
+            "indoor_humidity": env3_0.humidity,
+            "ip_address": ip,
+            "outdoor_temp": outdoor_temp,
+            "outdoor_humidity": outdoor_humidity,
+            "indoor_air_quality": gas_unit.TVOC,
+            "date": date,
+            "time": time
+        }
+    }
+    try:
+        url = "{}/send-to-bigquery".format(flask_url)
+        response = urequests.post(url, json=data)
+    except Exception as e:
+        print("Failed to send data to BigQuery:", str(e))
+    finally:
+        if response:
+            response.close()
 
 # Function to update sensor values and PIR state on the display
-def update_display():
+def update_sensor_display():
     env3_temp = env3_0.temperature 
     env3_hum = env3_0.humidity 
     gas_value = gas_unit.TVOC
@@ -102,13 +163,35 @@ def update_display():
     gas_label.set_text('Gas TVOC: {} ppb'.format(gas_value))
     motion_label.set_text('Motion Detected: ' + motion_detected)
 
-    current_weather = get_current_weather(get_public_ip())
+def update_api_display(current_weather):
     outdoor_hum_label.set_text('Outdoor Temp: {:.2f} C'.format(current_weather[0]))
     outdoor_temp_label.set_text('Outdoor Humidity: {:.2f} %'.format(current_weather[1]))
     imageplus0 = M5ImagePlus(200, 140, url='http://openweathermap.org/img/w/{}.png'.format(current_weather[2]), timer=True, interval=3000)
 
+# Fonction pour démarrer la boucle principale
+def main_loop():
+    global public_ip
+    api_last_update = time.time() - 300  # Force immediate update at start
 
-while True:
-    connect_wifi(wifi_credentials)
-    update_display()
-    time.sleep(60)
+    while True:
+        wlan = connect_wifi(wifi_credentials)
+        if wlan:
+            if public_ip is None:  # Obtient l'IP publique seulement si nécessaire
+                get_public_ip()
+            # Utilisez l'IP publique pour d'autres opérations ici, si nécessaire
+            update_sensor_display()
+            get_current_time()
+            if time.time() - api_last_update > 300:
+                current_weather = get_current_weather(public_ip)
+                update_api_display(current_weather)
+                api_last_update = time.time()
+
+                send_data(public_ip, current_weather[0], current_weather[1])
+
+        else:
+            IPLabel.set_text('Failed to connect to WiFi')
+            public_ip = None  # Réinitialisez l'IP publique si la connexion échoue
+        time.sleep(1)
+
+# Assurez-vous d'appeler main_loop() pour démarrer la boucle
+main_loop()
