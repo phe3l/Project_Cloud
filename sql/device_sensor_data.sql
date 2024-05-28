a9358b0e6ea1fe9559dc8333e3ca63a099ad146f
phe3l <146002891+phe3l@users.noreply.github.com>
2024-05-28 18:51:59 +0200

    add sql file

---
+++ sql/device_sensor_data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE device_sensor_data (
+    date DATE,
+    time TIME,
+    indoor_temp FLOAT,
+    indoor_humidity FLOAT,
+    indoor_air_quality FLOAT,
+    outdoor_temp FLOAT,
+    outdoor_humidity FLOAT,
+    ip_address VARCHAR(255)
+);
